ac2cfebff33e2ce432e6dde3f1d086bd230683f8
Thomas <tommyprezioso@gmail.com>
2022-04-22 12:06:51 -0400

    Initial Commit

---
+++ DeveloperStatusForApple/DeveloperStatusForAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperStatusForAppleApp.swift
+//  DeveloperStatusForApple
+//
+//  Created by Thomas Prezioso Jr on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperStatusForAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
