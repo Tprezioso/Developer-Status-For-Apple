a087aa5017b64cbd6879d7fa9a6a21bab5ee1e9d
Thomas <tommyprezioso@gmail.com>
2022-04-22 18:53:23 -0400

    first commit

---
+++ DeveloperStatusForApple/AppleDeveloperStatusView.swift
@@ -0,0 +1,63 @@
+//
+//  AppleDeveloperStatusView.swift
+//  DeveloperStatusForApple
+//
+//  Created by Thomas Prezioso Jr on 4/22/22.
+//
+
+import SwiftUI
+
+struct AppleDeveloperStatusView: View {
+    @StateObject var stateModel = AppleDeveloperStatusViewStateModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(width: 225, height: 500)
+            .onAppear{
+                Task { @MainActor in
+                     await stateModel.fetchData()
+                }
+            }
+    }
+}
+
+struct AppleDeveloperStatusView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleDeveloperStatusView()
+    }
+}
+
+class AppleDeveloperStatusViewStateModel: ObservableObject {
+    @Published var status = StatusComponents(services: [])
+    @Published var loading = false
+    @Published var error = false
+        
+        func fetchData() async {
+            Task { @MainActor in
+                loading = true
+                do {
+                    status = try await getStatus()
+                    loading = false
+                    self.error = false
+                } catch {
+                    print(error)
+                    self.error = true
+                    loading = false
+                }
+            }
+        }
+        
+        func getStatus() async throws -> StatusComponents {
+            guard let url = URL(string: "https://www.apple.com/support/systemstatus/data/developer/system_status_en_US.js") else { fatalError("Missing URL") }
+            let urlRequest = URLRequest(url: url)
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error while fetching data") }
+            let newData = data.subdata(in: Range(13...(data.count - 3)))
+            if let components = try? JSONDecoder().decode(StatusComponents.self, from: newData) {
+                print(">>\(components.services)")
+                return components
+            }
+            return StatusComponents(services: [])
+        }
+}
---
+++ DeveloperStatusForApple/StatusComponents.swift
@@ -0,0 +1,29 @@
+//
+//  StatusComponents.swift
+//  DeveloperStatusForApple
+//
+//  Created by Thomas Prezioso Jr on 4/22/22.
+//
+
+import Foundation
+
+struct Service: Codable {
+    var redirectUrl: String?
+    var events: [Event]
+    var serviceName: String
+
+}
+
+struct Event: Codable {
+    let usersAffected: String?
+    let epochStartDate, epochEndDate: Int?
+    let messageID, statusType, datePosted, startDate: String?
+    let endDate: String?
+    let affectedServices: String?
+    let eventStatus, message: String?
+
+}
+
+struct StatusComponents: Codable {
+    var services: [Service]
+}
